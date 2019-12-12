0e57dbb5cf9bf4b090e963a26bd036b093a377d3
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2019-12-12 01:38:28 -0500

    Fixed bug concerning index in localStorage, and added text content using API call

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,28 @@
 // Function for current weather
 function getWeather() {  
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $("#searchInput").val() + ",us&appid=ef5a1aac4687a7fff928e96832672dc8";
+    var queryUVURL = "api.openweathermap.org/data/2.5/uvi?q=" + $("#searchInput").val() + ",us&appid=ef5a1aac4687a7fff928e96832672dc8";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
+        $("#cityText").text(response.name);
+        var tempKelvin = response.main.temp;
+        var tempFahr = (((tempKelvin - 273.15) * 1.8) + 32);
+        $("#tempText").text("Temperature: " + tempFahr.toFixed() + "\xB0" + "F");
+        $("#humidityText").text("Humidity: " + response.main.humidity + "%");
+        $("#windText").text("Wind speed: " + response.wind.speed + "mph");
+    })
+
+    $.ajax({   // I keep getting the error that access is "blocked"
+        url: queryUVURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
     })
 }
-
-// getWeather();
 
 
 
@@ -18,10 +30,19 @@ function getWeather() {
 // My functions to "save" user city search
 var index = 0;
 
+function getIndex() {
+    if (localStorage.getItem("index") === null) {
+        index = 0;
+    }
+    else {
+        index = parseInt(localStorage.getItem("index"));
+    }
+}
+
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var searchResult = $("#searchInput").val().trim();
-    localStorage.getItem("index");
+    getIndex();
     localStorage.setItem("userInput" + index, searchResult);
     index = index + 1;
     localStorage.setItem("index", index);
