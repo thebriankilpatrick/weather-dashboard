5848359775e04e55781642a9bd5e56880915e33d
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2019-12-13 13:30:18 -0500

    Added some functionality for the 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,12 @@ function getWeather() {
     }).then(function(response) {
         console.log(response);
         $("#cityText").text(response.name);
+
         var iconCode = response.weather[0].icon;
         var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
         $("#iconContainer").append("<img id='iconImage'>");
         $("#iconImage").attr("src", iconURL);
+
         var tempKelvin = response.main.temp;
         var tempFahr = (((tempKelvin - 273.15) * 1.8) + 32);
         $("#tempText").text("Temperature: " + tempFahr.toFixed() + "\xB0" + "F");
@@ -29,6 +31,41 @@ function getWeather() {
         method: "GET"
         }).then(function(response) {
             $("#uVText").text("UV Index: " + response.value);
+        })
+
+        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + $("#searchInput").val() + ",us&appid=942ae6ca747eb423646036b5684169fa";
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function(response) {  
+            // FIND A WAY TO REMOVE OLD CARDS BEFORE APPLYING NEW ONES
+            // ALSO, THE LOCATION IS NOT PULLING CORRECTLY
+            // AS WELL AS THE FORECAST IMAGE
+            $("<div class='card text-white bg-info mb-3 float-left'>").remove();
+            console.log(response);
+            var arrayLength = response.list.length;
+            for (var i = 0; i < arrayLength; i += 8) {
+                // Create bootstrap cards
+                // create content
+                // append to row
+                var forecastCard = $("<div class='card text-white bg-info mb-3 float-left'>");
+                $(".forecastContainer").append(forecastCard); // See if float left works
+                var dt = response.list[i].dt_txt  // Grabbing date from forecast
+                var dateFixed = moment(dt, "YYYY.MM.DD").format("MM/DD/YYYY");  // Changing format of date, using moment.js
+                forecastCard.append($("<div class='card-header'>").text(dateFixed));
+    
+                var iconCode = response.list[i].weather[0].icon;
+                console.log(iconCode);
+                var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                forecastCard.append("<img class='forecastImage'>");
+                $(".forecastImage").attr("src", iconURL);
+                
+                var tempKelvin = response.list[i].main.temp;
+                var tempFahr = (((tempKelvin - 273.15) * 1.8) + 32);
+                forecastCard.append($("<p>").text("Temperature: " + tempFahr.toFixed() + "\xB0" + "F"));
+                forecastCard.append($("<p>").text("Humidity: " + response.list[i].main.humidity + "%"));
+            }
         })
     })
 }
@@ -70,7 +107,7 @@ function generateList() {
 }
 
 // This is to create a click handler on the saved city list
-// Clicked on a city in the list, will display that city's weather
+// Clicking on a city in the list, will display that city's weather
 $(document).on("click", ".list-group-item", function() {
     $("#searchInput").val($(this).text());
     getWeather();
